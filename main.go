feac09fcd859958a607829425145901258ed9a44
fida1989 <fida.muntaseer.1989@gmail.com>
2021-01-23 23:37:21 +0600

    initial commit

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 var num = 5
 
 func main() {
 	fmt.Println("Hello World")
-	num1 := 2
-	num2 := 4
-	fmt.Println(add(num1, num2))
+	//
+	var sum = add(5, 6)
+	fmt.Printf("Ourput is %d\n",sum)
+	//
+	con :=6
+	if(con > 5){
+		fmt.Println("yes")
+	}else{
+		fmt.Println("no")
+	}
+	//
+	num1 := 2.0
+	num2 := 4.0
 	square1, square2 := square(num1, num2)
 	fmt.Println(square1, square2)
+	//
 	for i := 1; i <= 5; i++ {
 		fmt.Println("Loop")
 	}
@@ -23,8 +35,8 @@ func add(x int, y int) int {
 	return result
 }
 
-func square(x int, y int) (int, int) {
-	var result1 = x * x
+func square(x float64, y float64) (float64, float64) {
+	var result1 = math.Sqrt(x)
 	var result2 = y * y
 	return result1, result2
 }
